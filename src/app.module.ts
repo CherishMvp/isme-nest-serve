4cc6cd43f14d99bd83498db2862aa9a226b6b284
zhangwt <lookcherish@163.com>
2024-01-18 12:20:33 +0800

    feat: 修復環境變量

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,9 +15,11 @@ import { RoleModule } from './modules/role/role.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ClassModule } from './modules/class/class.module';
 
-const envFilePath = ['.env.development', '.env.production', '.env']
+let envFilePath = ['.env.development', '.env.production', '.env'];
 const isDev = process.env.NODE_ENV === 'development';
-!isDev ? envFilePath.shift() : envFilePath
+
+if (!isDev) envFilePath.shift();
+
 console.log('isDev', isDev);
 console.log('envFilePath', envFilePath);
 @Module({
